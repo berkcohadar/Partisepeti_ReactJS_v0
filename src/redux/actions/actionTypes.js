64fc90fe08926e589ad0503a0c2d51c07a55fa64
Deger <adeger34@gmail.com>
2020-12-14 15:43:37 +0300

    delete

--- src/redux/actions/actionTypes.js
+++
@@ -1,9 +0,0 @@
-//aksiyon tiplerinin tanımlamaları burada yapılacak
-export const GET_MENU_ITEMS_SUCCESS="GET_MENU_ITEMS_SUCCESS"     
-
-
-export const CHANGE_CATEGORY="CHANGE_CATEGORY"
-
-export const GET_CATEGORIES_SUCCESS="GET_CATEGORIES_SUCCESS"
-
-export const GET_PRODUCTS_SUCCESS = "GET_PRODUCTS_SUCCESS"
